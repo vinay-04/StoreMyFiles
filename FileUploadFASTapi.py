f880286f752aec78a17dd79b32efb991c700918d
vinay-04 <vinayrajan04@gmail.com>
2023-03-20 19:21:40 +0530

    final

--- FileUploadFASTapi.py
+++ FileUploadFASTapi.py
@@ -1,11 +1,14 @@
 import pyrebase as pb
 import json
 from fastapi import FastAPI, HTTPException, UploadFile, File
+from fastapi.responses import HTMLResponse
 import uvicorn
+import os
 
 
 with open('config.json') as data:
     config = json.load(data)
+
 
 app = FastAPI(title="File Upload")
 
@@ -23,14 +26,21 @@ class CloudStorage:
     
     def initializeFirebaseStorage(self):
         self.storage = self.firebase.storage()
+        pass
 
     def uploadFile(self):
         try:
             @app.post('/file-upload')
             async def uploadFile(file:UploadFile = File(...)):
+                contents = file.file.read()
+                os.system(f"touch uploads/{file.filename}")
+                with open(f"uploads/{file.filename}", "wb") as f:
+                    f.write(contents)
+                    file.file.close()
                 self.file_type = file.filename.split('.').pop()
                 self.file_name = file.filename[:-len(self.file_type)-1]
-                self.storage.child(file.filename).put(file.filename)
+                self.storage.child(f"uploads/{file.filename}").put(f"uploads/{file.filename}")
+                os.system(f"rm uploads/{file.filename}")
                 return {"fileName":self.file_name, "fileType":self.file_type}
         except Exception:
             return HTTPException
